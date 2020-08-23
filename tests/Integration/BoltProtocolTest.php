00632b1bb03744253e6e01d981a0c9e9fc5b0368
ghlen <ghlen@pm.me>
2020-08-23 15:12:37 +0200

    added bolt handshake test for neo4j ^4.0

---
+++ tests/Integration/BoltProtocolTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the GraphAware Neo4j Client package.
+ *
+ * (c) GraphAware Limited <http://graphaware.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace GraphAware\Neo4j\Client\Tests\Integration;
+
+use GraphAware\Bolt\Exception\HandshakeException;
+use GraphAware\Neo4j\Client\ClientBuilder;
+use GraphAware\Neo4j\Client\ClientInterface;
+use GraphAware\Neo4j\Client\Exception\Neo4jException;
+use PHPUnit\Framework\TestCase;
+
+final class BoltProtocolTest extends TestCase
+{
+    private ClientInterface $client;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $boltUrl = sprintf(
+            'bolt://%s:%s@%s',
+            getenv('NEO4J_USER'),
+            getenv('NEO4J_PASSWORD'),
+            getenv('NEO4J_HOST')
+        );
+        $this->client = ClientBuilder::create()->addConnection('bolt', $boltUrl)->build();
+    }
+
+    public function testExceptionThrownWhenNeeded(): void
+    {
+        if ($this->isV4OrUp()) {
+            $this->expectException(HandshakeException::class);
+        }
+
+        $this->client->run('MATCH (x) RETURN x LIMIT 1');
+    }
+
+    private function isV4OrUp(): bool
+    {
+        return ((int) explode('.', getenv('NEO4J_VERSION'), 2)[0]) >= 4;
+    }
+}
